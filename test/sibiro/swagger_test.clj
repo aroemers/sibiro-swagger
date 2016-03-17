c2b9ad17b66ad205bb01f469fd1aac62977f1d18
Arnout Roemers <github@functionalbytes.nl>
2016-03-17 14:21:44 +0100

    Update tests with new responses default

--- test/sibiro/swagger_test.clj
+++ test/sibiro/swagger_test.clj
@@ -2,29 +2,40 @@
   (:require [clojure.test :refer :all]
             [sibiro.swagger :refer :all]))
 
+;;; Helper functions.
+
+(defn- default-operation [method & {:as rest}]
+  (assoc rest method {:responses {:default {:description "Default response."}}}))
+
+
+;;; The tests.
+
 (deftest test-basic-route
   (testing "basic unparameterized route"
     (let [routes #{[:get "/basic"]}]
-      (is (= {"/basic" {:get {} :parameters ()}}
+      (is (= {"/basic" (default-operation :get :parameters ())}
              (:paths (swaggerize routes)))))))
 
 (deftest test-parameterized-route
   (testing "basic parameterized route"
     (let [routes #{[:get "/basic/:id/*"]}]
-      (is (= {"/basic/{id}/{*}" {:get {} :parameters [{:name :id :type :string :in :path}
-                                                      {:name :* :type :string :in :path}]}}
+      (is (= {"/basic/{id}/{*}" (default-operation :get :parameters [{:name :id :type :string
+                                                                      :in :path :required true}
+                                                                     {:name :*  :type :string
+                                                                      :in :path :required true}])}
             (:paths (swaggerize routes)))))))
 
 (deftest test-parameter-at-start
   (testing "parameter at start, without slash"
     (let [routes #{[:get "*"]}]
-      (is (= {"/{*}" {:get {} :parameters [{:name :* :type :string :in :path}]}}
+      (is (= {"/{*}" (default-operation :get :parameters [{:name :*  :type :string
+                                                           :in :path :required true}])}
              (:paths (swaggerize routes)))))))
 
 (deftest test-base
   (testing "deep merge of base specification"
     (let [routes #{[:get "/basic"]}]
-      (is (= {:paths {"/basic" {:get {} :parameters ()}}
+      (is (= {:paths {"/basic" (default-operation :get :parameters ())}
               :info {:title "My test title"
                      :version "0.1-SNAPSHOT"}
               :swagger "2.0"}
@@ -33,15 +44,16 @@
 (deftest test-route-info
   (testing "route info specification"
     (let [routes #{[:get "/basic" {:swag {:description "My basic route"}}]}]
-      (is (= {"/basic" {:get {:description "My basic route"}
+      (is (= {"/basic" {:get {:description "My basic route"
+                              :responses {:default {:description "Default response."}}}
                         :parameters ()}}
              (:paths (swaggerize routes :route-info :swag)))))))
 
 (deftest test-expand-any
   (testing "expanding :any routes into all verbs"
     (let [routes #{[:any "/basic"]}]
-      (is (= {"/basic" {:get {} :put {} :post {} :delete {} :options {} :head {} :patch {}
-                        :parameters ()}}
+      (is (= {"/basic" (apply merge {:parameters ()}
+                              (map default-operation [:get :put :post :delete :options :head :patch]))}
              (:paths (swaggerize routes :route-info :swag)))))))
 
 (deftest test-json
